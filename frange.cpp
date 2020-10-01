f7835524c8e666e610141c5905257720d94ba920
Khusbu Kumari <35945640+khusbux@users.noreply.github.com>
2020-10-01 23:25:16 +0530

    Add files via upload

---
+++ frange.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+void fibo();
+int main()
+{
+	int n1=0,n2=1,sum=0,r1,r2,i,n;
+	cout<<"enter the number: ";
+	cin>>n;
+	/* cout<<n1<<"\n"<<n2<<endl;
+	for(i=2;i<r2;i++)
+	{
+		sum = n1+n2;
+		n1 =n2;
+		n2 = sum;
+	}
+	cout<<sum<<endl;
+	*/
+	
+	bool flag = false;
+		if(flag)
+	{
+		cout<<"yes, Fibonacci number";
+	}
+	else
+	{
+		cout<<"Not fibonacci number";
+	}
+	return 0;
+	
+	
+	
+}
+	void fibo( )
+	{
+		int sum,n1,n2,n
+			while(sum<=n)
+	{
+		if(sum==n)
+		{
+			flag = true;
+		}
+		sum = n1+n2;
+		n1 =n2;
+		n2 = sum;
+		
+	}
+	}
+	
+
+
